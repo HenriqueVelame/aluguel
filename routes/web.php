9f4cbafd068b58632819bdf129b8f0866e7f8490
KiwiComHIV <kaio88861@gmail.com>
2026-03-11 18:37:39 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Rota para a página inicial do catálogo (que você criou)
+Route::get('/cosplays', function () {
+    return view('cosplays.index'); // Certifique-se que o arquivo está em resources/views/cosplays/index.blade.php
+})->name('cosplays.index');
+
+// Rota para o formulário de cadastro (que você vai fazer)
+Route::get('/cosplays/novo', function () {
+    return view('cosplays.create');
+})->name('cosplays.create');
+
 Route::get('/', function () {
     return view('welcome');
 });
