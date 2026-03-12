c9aa1de4b60c7add9359618834c32eb4ae828b2a
KiwiComHIV <kaio88861@gmail.com>
2026-03-11 21:40:57 -0300

    fix - routes and models

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
+use App\Models\Locacao;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    protected $table = 'clientes';
+
     protected $fillable = ['nome', 'cpf', 'email', 'telefone', 'endereco', 'medidas_corpo'];
 
     public function locacoes()
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemCosplayController;
+use App\Http\Controllers\LocationController;
 
-// 1. Rota para a página inicial (Redireciona para a lista)
 Route::get('/', function () {
     return redirect()->route('cosplays.index');
 });
 
-// 2. Listagem (Página principal)
-Route::get('/cosplays', function () {
-    return view('cosplays.index');
-})->name('cosplays.index');
-
-// 3. Criar Novo (Formulário vazio)
-Route::get('/cosplays/novo', function () {
-    return view('cosplays.create');
-})->name('cosplays.create');
-
-// 4. Editar (Formulário preenchido)
-Route::get('/cosplays/editar', function () {
-    return view('cosplays.edit');
-})->name('cosplays.edit');
-
-// 5. Ver Detalhes (Página com todas as informações) --- FALTA ESSA!
-Route::get('/cosplays/detalhes', function () {
-    return view('cosplays.show');
-})->name('cosplays.show');
-
-// 6. Rota de Deletar (Ação no botão da lista) --- FALTA ESSA!
-// Nota: No futuro o Pessoa A vai mudar para Route::delete
-Route::get('/cosplays/deletar', function () {
-    return "Simulando exclusão...";
-})->name('cosplays.destroy');
+Route::resource('clientes', ClientController::class);
+Route::resource('categorias', CategoryController::class);
+Route::resource('cosplays', ItemCosplayController::class);
+Route::resource('locacoes', LocationController::class);
